7705970ab1ceb8de9e2ab56f7bc66e862955dd75
Ashutosh Thorat <ashutoshrit64@gmail.com>
2022-03-21 08:42:17 +0530

    pull for-loop branch

--- src/com/bridgelabz/ifElse/DisplayEven.java
+++ src/com/bridgelabz/ifElse/DisplayEven.java
@@ -40,7 +40,6 @@ public class DisplayEven {
 			break;
 		}
 		System.out.println("even number between range from 0 to "+number);
-
 		for(int i=0;i<number;i++) {
 			if(i%2==0) {
 				System.out.println(i);
